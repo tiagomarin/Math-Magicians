d9a208d78161ae1f72e3396c147820d3d129e107
Tiago Marin <tiago.lelinski@gmail.com>
2022-08-16 16:40:47 -0300

    add calculator component and basic styles

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div id="calculator-placeholder">
+        <h1>Tito&apos; Calculator</h1>
+        <div id="calculator">
+          <input type="text" name="input" className="btn" id="input" />
+          <input type="button" className="btn" value="AC" />
+          <input type="button" className="btn" value="+/-" />
+          <input type="button" className="btn" value="%" />
+          <input type="button" className="btn operator" value="÷" />
+          <input type="button" className="btn" value="7" />
+          <input type="button" className="btn" value="8" />
+          <input type="button" className="btn" value="9" />
+          <input type="button" className="btn operator" value="x" />
+          <input type="button" className="btn" value="4" />
+          <input type="button" className="btn" value="5" />
+          <input type="button" className="btn" value="6" />
+          <input type="button" className="btn operator" value="-" />
+          <input type="button" className="btn" value="1" />
+          <input type="button" className="btn" value="2" />
+          <input type="button" className="btn" value="3" />
+          <input type="button" className="btn operator" value="+" />
+          <input type="button" className="btn" value="0" />
+          <input type="button" className="btn" value="." />
+          <input type="button" className="btn operator" value="=" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
