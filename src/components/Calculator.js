f5cb4a7662ac31f4f5a0777575e54b43ffe6e445
Tiago Marin <tiago.lelinski@gmail.com>
2022-08-17 16:42:48 -0300

    add calculation functionality to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,47 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
+  onClickHandler = (e) => {
+    const result = calculate(this.state, e.target.value);
+    this.setState(result);
+  };
+
   render() {
+    const { total, next } = this.state;
     return (
       <div id="calculator-placeholder" className="calculator-placeholder">
         <h1>Tito&apos; Calculator</h1>
         <div id="calculator" className="calculator">
-          <input type="text" name="input" className="btn input" id="input" />
-          <input type="button" className="btn" value="AC" />
-          <input type="button" className="btn" value="+/-" />
-          <input type="button" className="btn" value="%" />
-          <input type="button" className="btn operator" value="÷" />
-          <input type="button" className="btn" value="7" />
-          <input type="button" className="btn" value="8" />
-          <input type="button" className="btn" value="9" />
-          <input type="button" className="btn operator" value="x" />
-          <input type="button" className="btn" value="4" />
-          <input type="button" className="btn" value="5" />
-          <input type="button" className="btn" value="6" />
-          <input type="button" className="btn operator" value="-" />
-          <input type="button" className="btn" value="1" />
-          <input type="button" className="btn" value="2" />
-          <input type="button" className="btn" value="3" />
-          <input type="button" className="btn operator" value="+" />
-          <input type="button" className="btn" value="0" />
-          <input type="button" className="btn" value="." />
-          <input type="button" className="btn operator" value="=" />
+          <input type="text" name="input" className="btn input" id="input" value={next || total} />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="AC" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="+/-" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="%" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="÷" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="7" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="8" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="9" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="x" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="4" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="5" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="6" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="-" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="1" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="2" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="3" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="+" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="0" />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="." />
+          <input onClick={this.onClickHandler} type="button" className="btn" value="=" />
         </div>
       </div>
     );
